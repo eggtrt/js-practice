6e88605bbefda0a837ff4b97107534c8cfb1b465
eggtrt <eggtrt@gmail.com>
2021-07-06 14:51:00 +0900

    calling

---
+++ this-concept.js
@@ -0,0 +1,21 @@
+function baz () {
+    // 호출 스택: 'baz'
+    // 호출부는 전역 스코프 내부
+    console.log('baz');
+    bar(); // 'bar의 호출부
+}
+
+function bar () {
+    // 호출 스택: baz -> bar
+    // 호출부는 baz 내부
+    console.log('bar');
+    foo(); // foo의 호출부
+}
+
+function foo () {
+    // 호출 스택: baz -> bar -> foo
+    // 호출부는 bar 내부
+    console.log('foo');
+}
+
+baz(); // baz의 호출부
--- this.js
+++ this.js
@@ -19,3 +19,26 @@ identify.call(me);
 identify.call(you);
 speak.call(me);
 speak.call(you);
+
+// 위와 같은 함수를 this 없이 만들기 위해선
+// function identify(context) {
+//     return context.name.toUpperCase();
+// }
+ 
+// function speak(context) {
+//     var greeting = "Hello, I,m " + identify(context);
+//     console.log(greeting);
+// }
+
+// var me = {
+//     name: "jisu"
+// };
+
+// var you = {
+//     name: "sowon"
+// };
+
+// identify(me);
+// identify(you);
+// speak(me);
+// speak(you);
