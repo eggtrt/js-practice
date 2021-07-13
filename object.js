d0f0c2c06c036e75ba0f0bad8d0a8df7a014f040
eggtrt <eggtrt@gmail.com>
2021-07-13 14:51:00 +0900

    computed property

--- object.js
+++ object.js
@@ -10,3 +10,26 @@ var myObj = {
 
 var myObj = new Object();
 myObj.key = value;
+
+// 3.2 Type
+
+// 객체가 아닌 것 (단순 원시 타입)
+// string, number, boolean, null, undefined
+// 객체 하위 타입 (복합 원시 타입)
+// function
+
+// 3.3 내장 객체
+
+// String, Number, Boolean, Object, Function, Array, Date, RegExp, Error
+// 생성자를 통해 객체 생성
+
+// 3.3 내용
+
+// 3.3.1 계산된 프로퍼티명
+
+var prefix = "foo";
+var myObject =  {
+    [prefix + "bar"]: "hello",
+    [prefix + "baz"]: "world" 
+}
+
