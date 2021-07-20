2f9be4714bdeb635c0fec09733f8ceb90b23ca80
eggtrt <eggtrt@gmail.com>
2021-07-20 14:51:00 +0900

    inherit

--- class.js
+++ class.js
@@ -15,4 +15,23 @@ class CoolGuy {
 Joe = new CoolGuy("카드 마술");
 Joe.showOff() // 마술!!: 카드마술
 
+// 4.2 클래스 상속
 
+class Vehicle {
+    engines = 1
+    ignition() {
+        output("엔진을 켠다"); 
+    }
+    drive() {
+        ignition();
+        output("방향을 맞추고 앞으로 간다")
+    }
+}
+
+class Car extends Vehicle {
+    wheels = 4
+    drive () {
+        inherited:drive()
+        ouput(wheels, "개의 바퀴로 굴러간다")
+    }
+}
