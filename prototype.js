28e81b225a587d449a32163078eb1e4658f81e6e
eggtrt <eggtrt@gmail.com>
2021-07-23 14:51:00 +0900

    inherit

--- prototype.js
+++ prototype.js
@@ -38,3 +38,28 @@ Object.defineProperty(Foo.prototype, "constructor", {
     configurable: false,
     value: Foo // Constructor가 Foo를 가리키게 함
 })
+
+// 5.3 상속
+
+function Foo(name) {
+    this.name = name;
+}
+
+Foo.prototype.myName = function() {
+    return this.name;
+}
+
+function Bar (name, label) {
+    Foo.call(this, name);
+    this.label = label;
+}
+
+Bar.prototype = Object.create(Foo.prototype);
+Bar.prototype.myLabel = function() {
+    return this.label;
+}
+
+var a = new Bar("a", "obj a");
+
+a.myName(); // "a"
+a.myLabel() // "obj a"
