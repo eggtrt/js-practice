9fd1208e16a314fdf8aad3deaf1fdde9784a9d59
eggtrt <eggtrt@gmail.com>
2021-07-31 14:51:00 +0900

    scheduling

--- promise.js
+++ promise.js
@@ -46,3 +46,24 @@ if (
 } else {
     //데너블이 아니다
 }
+
+// scheduling
+
+var p3 = new Promise(function(resolve, reject) {
+    resolve("B");
+});
+
+var p1 = new Promise(function(resolve, reject) {
+    resolve(p3);
+})
+
+p2 = new Promise(function(resolve, reject){
+    resolve("A");
+});
+
+p1.then((v) => {
+    console.log(v);
+});
+p2.then((v) => {
+    console.log(v);
+})
